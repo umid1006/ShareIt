287f46fc84e11135d0af1521dd57a2c1582aaea5
Umid <umidjoninagamov@yandex.ru>
2025-03-22 17:08:41 +0500

    First commit

--- src/main/java/ru/practicum/exception/NotFoundException.java
+++
@@ -1,8 +0,0 @@
-package ru.practicum.exception;
-
-public class NotFoundException extends RuntimeException { // Extend RuntimeException
-
-    public NotFoundException(String message) {
-        super(message);
-    }
-}
